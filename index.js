e648775fe18d2f6a45ee89b83c7cca8240c6ece3
nkr2747 <152243606+nkr2747@users.noreply.github.com>
2026-09-15 00:36:52 +0530

    Fix callbackController import in index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 });
 app.get("/auth/github", authController);
 
-app.get("/auth/github/callback", callbackControllerv2);
+app.get("/auth/github/callback", callbackController);
 
 app.use(auth);
 
