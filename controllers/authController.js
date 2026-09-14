7c768c24e9ec2fc17720a5c9c4d453414d1e41fd
nkr2747 <152243606+nkr2747@users.noreply.github.com>
2026-09-15 00:38:08 +0530

    Change GitHub authentication redirect URL
    Updated the redirect URL for GitHub authentication.

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,10 @@ const authController = (req, res) => {
   // sirf OAuth — hamesha yahi rahega
   console.log("Redirecting to GitHub for authentication...");
   res.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.BACKEND_URL}/auth/github/callback`,
+    `https://github.com/apps/nkrstorage/installations/new`,
   );
+  // res.redirect(
+  //   `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.BACKEND_URL}/auth/github/callback`,
+  // );
 }
 module.exports = authController;
